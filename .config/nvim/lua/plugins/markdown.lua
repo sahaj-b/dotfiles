f13d86315ef36b2ca04fed7c27cad3caf27cb802
sahaj-b <sahajb0606@gmail.com>
2025-11-09 20:30:00 +0530

    Updated

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -67,12 +67,15 @@ return {
         },
         markdown = {
           list_items = {
+            wrap = false,
             shift_width = 0,
             marker_minus = { add_padding = false },
             marker_plus = { add_padding = false },
             marker_star = { add_padding = false },
-            marker_dot = { add_padding = false },
-            marker_parenthesis = { add_padding = false },
+            -- marker_dot = { add_padding = false },
+            -- marker_parenthesis = { add_padding = false },
+            marker_dot = { enable = false },
+            marker_parenthesis = { enable = false }
           }
         },
         ft = { "markdown", "codecompanion", "avante" },
