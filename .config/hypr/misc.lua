87cf1b956d770c5d542bec7fe468f5ee60c20c98
sahaj-b <sahajb0606@gmail.com>
2026-05-11 15:54:46 +0530

    Updated

---
+++ .config/hypr/misc.lua
@@ -0,0 +1,15 @@
+hl.config({
+  misc = {
+    force_default_wallpaper = 0,
+    disable_splash_rendering = true,
+    disable_hyprland_guiutils_check = true,
+    middle_click_paste = false,
+    enable_anr_dialog = false,
+  },
+
+  xwayland = {
+    force_zero_scaling = true,
+  },
+
+})
+
